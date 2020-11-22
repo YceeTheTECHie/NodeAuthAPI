55dcc52da0e4558b98818776dc4624af614a466f
yceethetechie <waplurd1@gmail.com>
2020-11-22 19:02:01 +0500

    implented passion hashing

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,22 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../Schema/user")
+
+//salting function
+
+const saltpw = async (password) => {
+    return await bcrypt.hash(password,12)
+}
+
 exports.signup = async (req, res) => {
-    console.log("user schema");
+    const { email, password } = req.body;
+    try {
+        const newUser = await User.create({
+            email,
+            password:saltpw,
+        });
+    }
+    catch (err) {
+        res.status(401).json(err.message);
+    }
 }
